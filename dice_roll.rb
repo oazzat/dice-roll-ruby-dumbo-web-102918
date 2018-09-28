25134749925ac10ae583929112c40fab67eb1f5f
Omar A <oazzat19@gmail.com>
2018-09-28 04:18:25 +0000

    automatically backed up by learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,13 +3,6 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  puts rand(6)
-end
-
-counter = 0
-
-while counter<20 do
-  roll()
-  counter += 1
+  return 1 + rand(6)
 end
 
